513e7cc3228e8153e92b544d2dd0be656a2ac22a
Administrator <Administrator@192.168.81.135>
2017-07-08 22:37:49 +0900

    com

--- project_ver01_consulting_web/src/main/java/com/project/REST/consulting/controller/CosunltingController.java
+++ project_ver01_consulting_web/src/main/java/com/project/REST/consulting/controller/CosunltingController.java
@@ -1,8 +1,21 @@
 package com.project.REST.consulting.controller;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CosunltingController {
+import com.project.REST.consulting.Service.CosultingServiceInterface;
 
+@RestController
+@RequestMapping("/re")
+public class CosunltingController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CosunltingController.class);
+	
+	@Inject
+	private CosultingServiceInterface serviceOBJ;
+	
 }
