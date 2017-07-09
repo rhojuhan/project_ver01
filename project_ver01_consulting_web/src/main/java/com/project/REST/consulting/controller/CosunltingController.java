4963f79cff6b4331c839921b3745d4d21c55ca7f
Administrator <Administrator@192.168.81.135>
2017-07-09 23:10:02 +0900

    security

--- project_ver01_consulting_web/src/main/java/com/project/REST/consulting/controller/CosunltingController.java
+++ project_ver01_consulting_web/src/main/java/com/project/REST/consulting/controller/CosunltingController.java
@@ -5,6 +5,8 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.REST.consulting.Service.CosultingServiceInterface;
@@ -18,4 +20,13 @@ public class CosunltingController {
 	@Inject
 	private CosultingServiceInterface serviceOBJ;
 	
+	@RequestMapping(value="/se",method=RequestMethod.POST)
+	public String testRest(@RequestParam("id") String id , @RequestParam("pw") String pw)throws Exception{
+		System.out.println(id + ":" + pw);
+		return "success";
+	}
+	@RequestMapping(value="/te",method = RequestMethod.GET)
+	public void test()throws Exception{
+		System.out.println("hello");
+	}
 }
